ed2c85b81ed0d3650338d6c8f981bfd842a50a01
Sander van Leeuwen <sajvanleeuwen@gmail.com>
2023-09-16 18:46:01 +0200

    First version of words and sentences

--- deployment/upload-source.cmd
+++ deployment/upload-source.cmd
@@ -2,8 +2,8 @@
 set batchPath=%~dp0
 set destination="%batchPath%\..\pomdp-baselines.zip"
 if exist %destination% del %destination%
-"C:\Program Files\7-zip\7z.exe" a -tzip %destination% "%batchPath%\.." "%batchPath%\..\deployment" -mx0 -xr!logs* -xr!__pycache__ -xr!data -xr!.git
+"C:\Program Files\7-zip\7z.exe" a -tzip %destination% "%batchPath%\.." "%batchPath%\..\deployment" -mx0 -xr!logs* -xr!__pycache__ -xr!data -xr!.git -xr!wandb
 
 scp -i C:\Users\Sander\.ssh\delftblue_rsa %destination% sajvanleeuwen@login.delftblue.tudelft.nl:/home/sajvanleeuwen/pomdp-baselines.zip
 
-ssh delftblue "rm -rf /home/sajvanleeuwen/pomdp-baselines-new; unzip /home/sajvanleeuwen/pomdp-baselines.zip -d /home/sajvanleeuwen/pomdp-baselines-new; rm /home/sajvanleeuwen/pomdp-baselines.zip; exit"
+ssh delftblue "rm -rf /home/sajvanleeuwen/distance-test; unzip /home/sajvanleeuwen/pomdp-baselines.zip -d /home/sajvanleeuwen/distance-test; rm /home/sajvanleeuwen/pomdp-baselines.zip; exit"
