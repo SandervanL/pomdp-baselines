d3e20877741d7228fc4e1d4a9a5faea149e4d27d
Sander van Leeuwen <sajvanleeuwen@gmail.com>
2023-09-28 13:20:02 +0200

    Update embeddings to exclude unknown words and include word and sentence metadata

--- embeddings/real.py
+++ embeddings/real.py
@@ -1,4 +1,5 @@
 import json
+import os
 import re
 from dataclasses import dataclass
 from typing import Callable, Optional
@@ -7,6 +8,8 @@ import numpy as np
 import torch
 from torch import Tensor
 import dill
+
+SentenceList = tuple[list[tuple[str, str]], list[tuple[str, str]]]
 
 
 @dataclass
@@ -18,10 +21,11 @@ class MazeTask:
     blocked: bool
     task_type: int  # unique number for each type (high vs low, heavy vs light, etc)
     word: str
+    sentence: str
 
 
-def create_embeddings(
-    inputs: tuple[list[tuple[str, str]], list[tuple[str, str]]],
+def create_sentence_embedding(
+    inputs: SentenceList,
     embedder: Callable[[str], Tensor],
 ) -> list[MazeTask]:
     tasks = [
@@ -31,10 +35,32 @@ def create_embeddings(
             index == 0,
             index,
             word.strip().lower(),
+            sentence.strip().lower(),
         )
         for index, sentence_list in enumerate(inputs)
         for sentence, word in sentence_list
         if len(sentence.strip()) > 0
+    ]
+
+    return [task for task in tasks if task.embedding is not None]
+
+
+def create_word_embedding(
+    inputs: SentenceList, embedder: Callable[[str], Tensor]
+) -> list[MazeTask]:
+    unique_words = [{task[1] for task in tasks} for tasks in inputs]
+    tasks = [
+        MazeTask(
+            embedder(word.strip().lower()),
+            False,
+            index == 0,
+            index,
+            word.strip().lower(),
+            word.strip().lower(),
+        )
+        for index, sentence_list in enumerate(unique_words)
+        for word in sentence_list
+        if len(word.strip()) > 0
     ]
 
     return [task for task in tasks if task.embedding is not None]
@@ -92,16 +118,26 @@ def get_word2vec_embedding(sentence: str) -> Optional[Tensor]:
     return result
 
 
-def main(input_file: str, output_file: str, use_word2vec: bool = False):
-    with open(input_file, "r") as file:
-        inputs: tuple[list[tuple[str, str]], list[tuple[str, str]]] = json.load(file)
+def main(
+    folder: str, input_file: str, use_word2vec: bool = False, sentences: bool = True
+):
+    with open(os.path.join(folder, input_file), "r") as file:
+        inputs: SentenceList = json.load(file)
 
     embedder = get_word2vec_embedding if use_word2vec else get_simcse_embedding
-    tasks = create_embeddings(inputs, embedder)
-    with open(output_file, "wb") as file:
+    creator = create_sentence_embedding if sentences else create_word_embedding
+
+    tasks = creator(inputs, embedder)
+
+    output_file = f"{'sentences' if sentences else 'words'}_{'word2vec' if use_word2vec else 'simcse'}2.dill"
+    print(f"Saving to {output_file}")
+    with open(os.path.join(folder, output_file), "wb") as file:
         dill.dump(tasks, file)
 
 
 if __name__ == "__main__":
-    # main("light_heavy_sentences.json", "light_heavy_word2vec_sentences.dill", True)
-    main("light_heavy_sentences.json", "light_heavy_simcse_sentences.dill", False)
+    folder = "embeddings/light_vs_heavy"
+    input_file = "sentences.json"
+    for use_word2vec in [True, False]:
+        for sentences in [True, False]:
+            main(folder, input_file, use_word2vec, sentences)
